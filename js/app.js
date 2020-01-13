cc6be8732886cd3108d5001545ae41c9e75e1d10
emus32 <gagaidoe1@gmail.com>
2020-01-13 22:38:31 +0100

    add card was change

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.getElementById("clear-cart").addEventListener("click", function(event){
+
+    let element;
+    element=event;
+    element=event.target;
+    element=event.target.id;
+    element=event.target.innerText=2+2;
+
+    console.log("element");
+});
+
